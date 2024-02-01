d11b1401c521804936c26e03b46b23010a8c2795
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-02-01 17:17:52 +0000

    fix: adding the spring boot

--- swapnil-srivastava/src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilSrivastavaApplication.java
+++ swapnil-srivastava/src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilSrivastavaApplication.java
@@ -1,13 +1,13 @@
-package eu.swapnilsrivastava.demo;
+package eu.swapnilsrivastava.swapnilsrivastava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SwapnilSrivastavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SwapnilSrivastavaApplication.class, args);
 	}
 
 }
--- swapnil-srivastava/src/test/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilSrivastavaApplicationTests.java
+++ swapnil-srivastava/src/test/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilSrivastavaApplicationTests.java
@@ -1,10 +1,10 @@
-package eu.swapnilsrivastava.demo;
+package eu.swapnilsrivastava.swapnilsrivastava;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SwapnilSrivastavaApplicationTests {
 
 	@Test
 	void contextLoads() {
