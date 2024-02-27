a7a13a432c28ef1a1d9d787723b9e41b0306316e
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-02-27 13:54:54 +0000

    fix: executing the code in main

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilSrivastavaApplication.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilSrivastavaApplication.java
@@ -3,11 +3,18 @@ package eu.swapnilsrivastava.swapnilsrivastava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import eu.swapnilsrivastava.swapnilsrivastava.services.SpeakerService;
+import eu.swapnilsrivastava.swapnilsrivastava.services.SpeakerServiceImp;
+
 @SpringBootApplication
 public class SwapnilSrivastavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwapnilSrivastavaApplication.class, args);
+
+		SpeakerService service = new SpeakerServiceImp();
+
+		System.out.println(service.findAll().get(0).getFirstName());
 	}
 
 }
