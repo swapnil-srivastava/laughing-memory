4f9c96a5e4e39ed81f7f6a6f7260799251ab4c72
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-03-05 13:34:40 +0000

    adding the speaker entity

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/model/Speaker.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/model/Speaker.java
@@ -1,24 +1,64 @@
 package eu.swapnilsrivastava.swapnilsrivastava.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name="speakers")
 public class Speaker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long speaker_id;
+    private String first_name;
+    private String last_name;
+    private String title;
+    private String company;
+    private String speaker_bio;
+
+    public Speaker() {
     
-    private String firstName;
-    private String lastName; 
+    }
     
-    public String getFirstName() {
-        return firstName;
+    public String getFirst_name() {
+        return first_name;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getLast_name() {
+        return last_name;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getSpeaker_bio() {
+        return speaker_bio;
+    }
+
+    public void setSpeaker_bio(String speaker_bio) {
+        this.speaker_bio = speaker_bio;
     }
 
 }
