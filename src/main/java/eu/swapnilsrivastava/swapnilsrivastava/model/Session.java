358d3e537c0058a4078341c93218840ace478b05
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-03-05 13:34:27 +0000

    adding the session entity

---
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/model/Session.java
@@ -0,0 +1,46 @@
+package eu.swapnilsrivastava.swapnilsrivastava.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name="sessions")
+public class Session {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long session_id;
+    private String session_name;
+    private String session_description;
+    private Integer session_length;
+
+    public String getSession_name() {
+        return session_name;
+    }
+
+    public void setSession_name(String session_name) {
+        this.session_name = session_name;
+    }
+
+    public String getSession_description() {
+        return session_description;
+    }
+
+    public void setSession_description(String session_description) {
+        this.session_description = session_description;
+    }
+
+    public Integer getSession_length() {
+        return session_length;
+    }
+
+    public void setSession_length(Integer session_length) {
+        this.session_length = session_length;
+    }
+
+    public Session() {
+
+    }
+
+}
