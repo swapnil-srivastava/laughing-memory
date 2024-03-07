f66f38db69fd2377f65b7238df664de9087ae802
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-03-07 12:58:03 +0000

    adding the CRUD operation for speaker repository

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/controller/SpeakerController.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/controller/SpeakerController.java
@@ -2,6 +2,7 @@ package eu.swapnilsrivastava.swapnilsrivastava.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import eu.swapnilsrivastava.swapnilsrivastava.model.Speaker;
@@ -9,6 +10,9 @@ import eu.swapnilsrivastava.swapnilsrivastava.repository.SpeakerRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @RestController
@@ -38,6 +42,15 @@ public class SpeakerController {
     public void delete(@RequestParam Long id) {
         // need to check the childern record for the speaker
         speakerRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public Speaker updatSpeaker(@PathVariable Long id, @RequestBody Speaker speaker) {
+        // In case of put we are replacing all the values of speaker object but if it was a PATCH we only would need to all the key which is nesscary. 
+        // TODO: Adding the validation for each key and return 400 if the value is wrong 
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker);
+        return speakerRepository.saveAndFlush(speaker);
     }
     
 }
