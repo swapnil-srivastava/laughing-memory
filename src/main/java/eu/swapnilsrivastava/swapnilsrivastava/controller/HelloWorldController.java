6b80129f55d54172f233623a4b1f06ae8b2c2f51
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-03-07 13:44:59 +0000

    adding the custome properties in the application.properties file and using it in the controller to reflect.

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/controller/HelloWorldController.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/controller/HelloWorldController.java
@@ -1,19 +1,33 @@
 package eu.swapnilsrivastava.swapnilsrivastava.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class HelloWorldController {
     
+    @Value("${app.version}")
+    private String appVersion; 
+
     @GetMapping("/hello-spring")
     public String HelloWorldSprings() {
         return "Hello World Spring";
     }
 
     @GetMapping("/hello-kafka")
-    public void HelloKafka() {
-        
+    public String HelloKafka() {
+        return "Hello World Kafka";   
     }
+
+    @GetMapping("/version")
+    public Map getAppVersion() {
+        Map map = new HashMap();
+        map.put("app-version", appVersion);
+        return map;
+    }
+    
 }
